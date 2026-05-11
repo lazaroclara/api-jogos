cbb51b8cb317d4c31ed1a5fc1569bb953b365d14
lazaroclara <161092938+lazaroclara@users.noreply.github.com>
2026-05-11 20:20:42 -0300

    login retorna uuid

--- routes/postLogin.js
+++ routes/postLogin.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { randomUUID } = require('crypto');
 
 router.post('/login', (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
+
     if (email === "usuario@esoft.com" && password === "Abc123") {
         return res.status(200).json({
-            mensagem: "Login bem-sucedido!"
+            token: randomUUID()
         });
     }
 
     res.status(401).json({
         mensagem: "Credenciais inválidas!"
-    }); 
+    });
 });
 
 module.exports = router;
