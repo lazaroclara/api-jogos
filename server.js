367cb8046e7505ce5418bb05a47bb1d35b52fde9
lazaroclara <161092938+lazaroclara@users.noreply.github.com>
2026-05-11 18:08:51 -0300

    corrigindo deploy

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-const postLogin = require('./routes/postLogin');
-const jogos = require('./routes/jogos');
+const jogosRoutes = require('./routes/jogos');
 
 app.use(express.json());
 
-app.use('/', postLogin);
-
-app.use('/', jogos);
+app.use('/', jogosRoutes);
 
 app.get('/', (req, res) => {
     res.send('API funcionando');
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('Servidor rodando');
