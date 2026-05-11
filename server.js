0d8d5daeaacfd97fa450a93be12f2a1a96949387
lazaroclara <161092938+lazaroclara@users.noreply.github.com>
2026-05-11 20:06:06 -0300

    debug logs

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 const jogosRoutes = require('./routes/jogos');
+const loginRoutes = require('./routes/postLogin');
 
 app.use(express.json());
 
 app.use('/', jogosRoutes);
+app.use('/', loginRoutes);
 
 app.get('/', (req, res) => {
     res.send('API funcionando');
@@ -15,4 +17,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`PORT env: ${process.env.PORT}`);
+    console.log(`DATABASE_URL definida: ${!!process.env.DATABASE_URL}`);
 });
