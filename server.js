5256f5a96372f67bebd9dfb7a2d6942c5fd02c72
lazaroclara <161092938+lazaroclara@users.noreply.github.com>
2026-05-24 15:35:05 -0300

    fix: adicionar cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const jogosRoutes = require('./routes/jogos');
 const loginRoutes = require('./routes/postLogin');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/', jogosRoutes);
